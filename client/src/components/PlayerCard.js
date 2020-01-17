9c1f620c1ffab7f35ae2acf98b7af8c9186bd4c0
mlazzell17@gmail.com <MicheSi>
2020-01-17 10:37:59 -0700

    ran app test to make sure tests working

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -3,9 +3,9 @@ import React from 'react';
 const PlayerCard = props => {
     return (
         <div className='playerCard'>
-            <h2>{props.name}</h2>
-            <p>Country: {props.country}</p>
-            <p>Number of Searches: {props.searches}</p>
+            <h2 data-testid='playerName'>{props.name}</h2>
+            <p data-testid='countryName'>Country: {props.country}</p>
+            <p data-testid='searchNumber'>Number of Searches: {props.searches}</p>
         </div>
     )
 }
